0669c45eab00febd2b01812a37322b7fbb21c622
MilanC994 <cabo.milan@yahoo.com>
2020-03-02 23:19:44 +0100

    Updates

---
+++ precache-manifest.6a831a71e7559595926f1e8727627349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ac60106d1c4e93dcc2ffeb955c8fa4",
+    "url": "/MoviesReact/index.html"
+  },
+  {
+    "revision": "f5771cb2039b0e559847",
+    "url": "/MoviesReact/static/css/main.d123ce66.chunk.css"
+  },
+  {
+    "revision": "36be9ad7c1beb4adc7c1",
+    "url": "/MoviesReact/static/js/2.b7b79e3a.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/MoviesReact/static/js/2.b7b79e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5771cb2039b0e559847",
+    "url": "/MoviesReact/static/js/main.041d066a.chunk.js"
+  },
+  {
+    "revision": "c86052bc114a77ad4535",
+    "url": "/MoviesReact/static/js/runtime-main.ff742ac9.js"
+  }
+]);
